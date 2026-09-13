c5ac086f418b925d61e2e2d59f7c9ebacaeae2d6
heavycaffeiner <146043537+heavycaffeiner@users.noreply.github.com>
2026-09-13 23:21:55 +0900

    chore(release): bump version to 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 36
-        versionCode = 10_03_04
-        versionName = "1.3.4"
+        versionCode = 10_03_05
+        versionName = "1.3.5"
 
         vectorDrawables {
             useSupportLibrary = true
